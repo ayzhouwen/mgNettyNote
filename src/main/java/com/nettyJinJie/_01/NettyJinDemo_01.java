488ee052d9f10ac3b05ddac4cdcd144a823ebd40
zhouwen <644988358@qq.com>
2022-02-20 10:31:50 +0800

    <<netty进阶>>服务器启动闪退问题

---
+++ src/main/java/com/nettyJinJie/_01/NettyJinDemo_01.java
@@ -0,0 +1,49 @@
+package com.nettyJinJie._01;
+
+import com.nettyJinJie.common.RecvHanldler;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
+
+//1.1 Netty服务端意外退出问题
+//案例1通过阻塞方式绑定监听端口,启动服务端之后，没发生任何异常,程序退出，代码示例如下:
+@Slf4j
+public class NettyJinDemo_01 {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup=new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup=new NioEventLoopGroup();
+        try {
+
+            ServerBootstrap b=new ServerBootstrap();
+            b.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG,100)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p=ch.pipeline();
+                            p.addLast(new RecvHanldler());
+                        }
+                    });
+
+         ChannelFuture f=   b.bind(18080).sync();//用同步方式绑定服务监听端口
+            f.channel().closeFuture().addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture future) throws Exception {
+                    //业务逻辑代码 ....
+                    bossGroup.shutdownGracefully();
+                    workerGroup.shutdownGracefully();
+                    log.info(future.channel().toString()+" 链路关闭");
+                }
+            });
+        } finally {
+//            bossGroup.shutdownGracefully();
+//            workerGroup.shutdownGracefully();
+        }
+
+
+    }
+}
